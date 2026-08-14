c600ff39ce481fb1887959149cb4e072ffd56815
hari163 <vinayakav820@gmail.com>
2026-08-14 12:46:05 +0530

    Initial commit

---
+++ src/main/java/com/college/fee/CollegeFeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.college.fee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeFeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeFeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/college/fee/CollegeFeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.fee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeFeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
